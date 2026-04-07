09eb7f0f086fa947f55050db69f2760751001130
pedro miranda <161866585+pedromiranda01@users.noreply.github.com>
2026-04-07 16:13:16 -0300

    Add files via upload

--- api-teste/api.php
+++ api-teste/api.php
@@ -2,15 +2,31 @@
     //cabecalho
     header("Content-Type: application/json");
 
+    $metodo = $_SERVER['REQUEST_METHOD'];
+
+    //echo "Método de requisição: ".$metodo;
+
+    switch($metodo){
+        case 'GET' :
+            echo "AQUI AÇÕES DO METODO GET";
+            break;
+        case 'POST' :
+            echo "AQUI AÇÕES DO METODO POST";
+            break;
+        default:
+            echo "MÉTODO NÃO ENCONTRADO!";
+            break;       
+    }
+
 
     //conteudo
-    $usuarios = [
-        ["id" =>1, "nome" => "Maria", "email" => "maria@gmail.com"],
-        ["id" =>2, "nome" => "Joao", "email" => "joao@gmail.com"]
-    ];
+    // $usuarios = [
+    //     ["id" =>1, "nome" => "Maria", "email" => "maria@gmail.com"],
+    //     ["id" =>2, "nome" => "Joao", "email" => "joao@gmail.com"]
+    // ];
 
-    //converte para json e retorna
-    echo json_encode($usuarios);
+    // //converte para json e retorna
+    // echo json_encode($usuarios);
 
 
 ?>
