c2c627de106655c4569f97b990b5f42d3a97d630
pedro miranda <161866585+pedromiranda01@users.noreply.github.com>
2026-04-14 16:08:18 -0300

    Add files via upload

--- api-teste/api.php
+++ api-teste/api.php
@@ -1,18 +1,49 @@
 <?php
     //cabecalho
-    header("Content-Type: application/json");
+    header("Content-Type: application/json; charset=UTF-");
 
     $metodo = $_SERVER['REQUEST_METHOD'];
 
-    //echo "Método de requisição: ".$metodo;
+    $arquivo = 'usuarios.json';
+
+    if (!file_exists($arquivo)) {
+        file_put_contents($arquivo, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
+
+    $usuarios = json_decode(file_get_contents($arquivo),true);
+
+    // $usuarios = [
+    //     ["id" =>1, "nome" => "Maria", "email" => "maria@gmail.com"],
+    //     ["id" =>2, "nome" => "Joao", "email" => "joao@gmail.com"]
+    // ];
 
     switch($metodo){
         case 'GET' :
-            echo "AQUI AÇÕES DO METODO GET";
+            //echo "AQUI AÇÕES DO METODO GET";
+            echo json_encode($usuarios);
             break;
         case 'POST' :
-            echo "AQUI AÇÕES DO METODO POST";
+            //echo "AQUI AÇÕES DO METODO POST";
+            $dados = json_decode(file_get_contents('php://input'),true);
+            //print_r($dados);
+            $novoUsuario = [
+                "id" => $dados["id"],
+                "nome" => $dados["nome"],
+                "enail" => $dados["email"],
+            ];
+
+            array_push($usuarios, $novoUsuario);
+            echo json_encode('Usuário inserido com sucesso!');
+            print_r($usuarios);
+
             break;
+        case 'PUT' :
+            echo "AQUI AÇÕES DO METODO PUT";
+            break;
+        case 'DELETE' :
+            echo "AQUI AÇÕES DO METODO DELETE#";
+            break;
+
         default:
             echo "MÉTODO NÃO ENCONTRADO!";
             break;       
